587774401a15d66e723b3b935da05bc3394466b9
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-01-18 00:52:38 +0500

    configure types

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,12 @@
+export interface User {
+    name: string;
+    email: string;
+  }
+  
+  export interface Contact {
+    _id: any;
+    name: string;
+    email: string;
+    phone: string;
+  }
+  
