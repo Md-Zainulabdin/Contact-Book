4cecbab0f8c96a91dae57d9c91a1d38bb7157b0e
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-01-17 23:58:02 +0500

    setup all routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,12 +19,12 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // routes
 
-// import authRoutes from "./routes/auth.routes";
-// import userRoutes from "./routes/user.routes";
-// import contactRoute from "./routes/contact.routes";
+import authRoutes from "./routes/auth.routes";
+import userRoutes from "./routes/user.routes";
+import contactRoute from "./routes/contact.routes";
 
-// app.use("/api/v1", userRoutes);
-// app.use("/api/v1", authRoutes);
-// app.use("/api/v1", contactRoute);
+app.use("/api/v1", userRoutes);
+app.use("/api/v1", authRoutes);
+app.use("/api/v1", contactRoute);
 
 export { app };
