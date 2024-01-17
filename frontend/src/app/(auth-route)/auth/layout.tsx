48a96e11d1e05c01270bf1ee4bc0b8ce3df48ab8
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-01-18 00:53:13 +0500

    add authorization in frontend with cookies

---
+++ frontend/src/app/(auth-route)/auth/layout.tsx
@@ -0,0 +1,16 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  const cookie = cookies();
+  
+  if (cookie.get("auth_token")) {
+    console.log(cookie.get("auth_token"));
+    
+    redirect("/contacts");
+  }
+
+  return <div>{children}</div>;
+};
+
+export default AuthLayout;
